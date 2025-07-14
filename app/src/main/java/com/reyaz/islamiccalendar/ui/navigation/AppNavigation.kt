4a15c8b236bdcfabdb1b62fdd1e1ce0b25136844
sadiquereyaz <mdsadique47@gmail.com>
2025-07-14 11:44:26 +0530

    Initial commit

---
+++ app/src/main/java/com/reyaz/islamiccalendar/ui/navigation/AppNavigation.kt
@@ -0,0 +1,7 @@
+package com.reyaz.islamiccalendar.ui.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavigation() {
+}
---
+++ app/src/main/java/com/reyaz/islamiccalendar/ui/screen/calendar/CalendarScreen.kt
@@ -0,0 +1,10 @@
+package com.reyaz.islamiccalendar.ui.screen.calendar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalendarScreen(
+    modifier: Modifier = Modifier
+) {
+}
